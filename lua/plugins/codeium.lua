eb4b47667e91b4ddd415aa4712bbf0e36dce5e10
7c00 <i@7c00.cc>
2024-11-08 14:16:35 +0800

    Update

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "Exafunction/codeium.nvim",
+  opts = {
+    virtual_text = {
+      enabled = true,
+      key_bindings = {
+        accept = "<Enter>",
+      },
+    },
+  },
+}
