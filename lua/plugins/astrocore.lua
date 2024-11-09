60e664d0eab1bccbef6321b23232c6f77d88f451
7c00 <i@7c00.cc>
2024-11-09 12:46:34 +0800

    Update

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -7,7 +7,6 @@ return {
 			g = {
 				clipboard = {
 					copy = { ["+"] = require("vim.ui.clipboard.osc52").copy("+") },
-					paste = { ["+"] = require("vim.ui.clipboard.osc52").paste("+") },
 				},
 			},
 		},
