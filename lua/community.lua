c9a59ef4de1a1fc5516ef8ed6a16d56ca33b6015
7c00 <i@7c00.cc>
2025-04-18 17:39:51 +0800

    Update

--- lua/community.lua
+++ lua/community.lua
@@ -2,9 +2,9 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.dart" },
-  -- { import = "astrocommunity.pack.typescript-deno" },
+  -- { import = "astrocommunity.pack.tailwindcss" },
+  -- { import = "astrocommunity.pack.dart" },
+  -- { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.pack.go" },
   -- { import = "astrocommunity.completion.codeium-nvim" },
   { import = "astrocommunity.colorscheme.dracula-nvim" },
