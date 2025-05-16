5db73724716a05e67b3a790a6e2a14f83814dcdd
7c00 <i@7c00.cc>
2025-05-16 15:26:51 +0800

    v5

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,18 +1,18 @@
 ---@type LazySpec
 return {
-	"AstroNvim/astrocore",
-	---@type AstroCoreOpts
-	opts = {
-		options = {
-			opt = {
-				relativenumber = false,
-				number = false,
-			},
-			g = {
-				-- clipboard = {
-				-- 	copy = { ["+"] = require("vim.ui.clipboard.osc52").copy("+") },
-				-- },
-			},
-		},
-	},
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      opt = {
+        relativenumber = false,
+        number = false,
+      },
+      -- g = {
+      --   clipboard = {
+      --     copy = { ["+"] = require("vim.ui.clipboard.osc52").copy("+") },
+      --   },
+      -- },
+    },
+  },
 }
