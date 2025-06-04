d703a5a3b4c9a837132d5295f8b856c91f453e45
7c00 <i@7c00.cc>
2025-06-04 17:11:50 +0800

    Update

--- lua/community.lua
+++ lua/community.lua
@@ -2,12 +2,13 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.python" },
   -- { import = "astrocommunity.pack.tailwindcss" },
   -- { import = "astrocommunity.pack.dart" },
   -- { import = "astrocommunity.pack.typescript" },
   -- { import = "astrocommunity.pack.rust" },
   -- { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.search.grug-far-nvim" },
   { import = "astrocommunity.recipes.ai" },
 }
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,6 +2,7 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    source_selector = { winbar = false },
     window = { width = 40 },
   },
 }
