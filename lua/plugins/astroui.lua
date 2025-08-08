6a58965b0d836bedd10b8b4198029031c537eb66
7c00 <i@7c00.cc>
2025-08-08 21:55:17 +0800

    Update

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -3,6 +3,6 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    colorscheme = "rose-pine",
+    colorscheme = "everforest",
   },
 }
---
+++ lua/polish.lua
@@ -0,0 +1,3 @@
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+-- vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
+-- vim.cmd("hi StatusLine guibg=NONE ctermbg=NONE")
