915208c95d8b1f29bd3567b0bed05ca0adc88cae
7c00 <i@7c00.cc>
2025-09-27 01:08:26 +0800

    Update neotree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,8 +1,20 @@
 ---@type LazySpec
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    source_selector = { winbar = false },
-    window = { width = 40 },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	opts = {
+		source_selector = { winbar = false },
+		window = { width = 40 },
+		default_component_configs = {
+			icon = {
+				folder_closed = "+",
+				folder_open = "-",
+				folder_empty = "+",
+				folder_empty_open = "-",
+				default = " ",
+				provider = function(icon, _, _)
+					return icon
+				end,
+			},
+		},
+	},
 }
