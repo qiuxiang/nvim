efc89a89ec812e1290e4ffb02ecd2807ff30a365
7c00 <i@7c00.cc>
2026-03-03 10:29:15 +0800

    fix: disable indent guides in terminal

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,3 +1,11 @@
 vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 -- vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
 -- vim.cmd("hi StatusLine guibg=NONE ctermbg=NONE")
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    -- 禁用 indent-blankline v3
+    vim.b.ibl_exclude = true
+  end,
+})
